b68bf87577df536fcc3c4f105ebaed7001a85eca
Jiří Čech <jiricech94@gmail.com>
2019-03-06 00:18:40 +0100

    Configure turbolinks

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,4 +3,7 @@ import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 const application = Application.start();
 const context = require.context("controllers", true, /_controller\.js$/);
+const turbolinks = require("turbolinks");
+
 application.load(definitionsFromContext(context));
+turbolinks.start();
