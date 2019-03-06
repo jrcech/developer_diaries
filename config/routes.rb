0a4a1df67c200a9850a72101e0485654d69e1a5b
Jiří Čech <jiricech94@gmail.com>
2019-03-07 00:45:14 +0100

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+
   scope :frontend_test do
     root to: 'frontend_test#index', as: :frontend_test
   end
+
+  root to: 'home#index'
 end
