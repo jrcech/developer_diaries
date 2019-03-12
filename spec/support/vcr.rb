f9d4a989a53067ea3b635d7a481217390284281d
Jiří Čech <jiricech94@gmail.com>
2019-03-12 19:18:51 +0100

    Set filtering of sensitive data for vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,4 +5,8 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
+
+  Dotenv.parse('.env', '.env.test').each do |k, v|
+    config.filter_sensitive_data("ENV[#{k}]") { v }
+  end
 end
