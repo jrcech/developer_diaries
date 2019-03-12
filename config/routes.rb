0757d3260ed751ea5739744a78f139b25c4630b7
Jiří Čech <jiricech94@gmail.com>
2019-03-12 23:44:34 +0100

    Generate admin dashboard controller
      - Generate admin_dashboard_controller with index action
    
      - Set root of admin to dashboard#index
    
      - Create shared_examples directoy
    
      - Create share get_examples for requests spec
    
      - Refactor frontend_test_spec to use shared get_examples
    
      - Refactor dashboard_spec to use shared get_examples
    
      - Disable obsolete rails best practices rules

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :admin do
+    root to: 'dashboard#index'
+  end
+
   scope :frontend_test do
     root to: 'frontend_test#index', as: :frontend_test
   end
