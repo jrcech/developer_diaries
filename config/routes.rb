2dbc3a47c80868609c9041fb2d44db03b508dc09
Jiří Čech <jiricech94@gmail.com>
2019-03-16 21:50:54 +0100

    Configure locale functionality
      - Add available locales to locale initializer
    
      - Configure routes to support localization
    
      - Set localization in application controller
    
      - Change reek configuration for application controller locale method

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  devise_for :users,
+             only: :omniauth_callbacks,
+             controllers: {
+               omniauth_callbacks: 'users/omniauth_callbacks'
+             }
 
-  namespace :admin do
-    get :frontend_test, to: 'frontend_test#index'
-    root to: 'dashboard#index'
+  scope '(:locale)', locale: /en|cs/ do
+    devise_for :users, skip: :omniauth_callbacks
+
+    namespace :admin do
+      get :frontend_test, to: 'frontend_test#index'
+      root to: 'dashboard#index'
+    end
+
+    get '/:locale', to: 'admin/dashboard#index'
+    root to: 'admin/frontend_test#index'
   end
-
-  root to: 'admin/frontend_test#index'
 end
