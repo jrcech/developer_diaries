1f09f878b648dcb9bf439cd1123efe6bf38967de
Jiří Čech <jiricech94@gmail.com>
2019-03-28 21:49:19 +0100

    Configure staging environment for capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+set :stage, :staging
+set :rails_env, :production
+set :branch, 'develop'
+
+server '46.101.110.210',
+       port: '26398',
+       user: 'jiricech',
+       roles: %w[app db web]
