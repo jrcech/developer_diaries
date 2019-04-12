dbbc284ace2ed6331518f44017b54e3a087177a7
Jiří Čech <jiricech94@gmail.com>
2019-04-12 16:53:53 +0200

    Add puma bins to deploay.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{rbenv_path} " \
                    "RBENV_VERSION=#{fetch(:rbenv_ruby)} " \
                    "#{rbenv_path}/bin/rbenv exec"
-set :rbenv_map_bins, %w[rake gem bundle ruby rails]
+set :rbenv_map_bins, %w[rake gem bundle ruby rails puma pumactl]
 set :rbenv_roles, :all
 
 # Nodenv
