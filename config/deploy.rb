d38b502e95e5648c483a890f837aa7ca95d31a09
Jiří Čech <jiricech94@gmail.com>
2019-04-12 20:20:38 +0200

    Fix webpack assets compiling

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,6 +92,15 @@ namespace :deploy do
     end
   end
 
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+
   before :starting, :check_revision
   after :finishing, :compile_assets
   after :finishing, :cleanup
