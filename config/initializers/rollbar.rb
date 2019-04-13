166d14312fe3f7eb56b73bea75ed3807363dfdf9
Jiří Čech <jiricech94@gmail.com>
2019-04-13 18:29:41 +0200

    Comment rollbar initializer

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-Rollbar.configure do |config|
-  config.access_token = '8722b03246844dfb92645a84c84bd061'
-
-  config.enabled = false if Rails.env.test?
-
-  config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
-end
+# Rollbar.configure do |config|
+#   config.access_token = '8722b03246844dfb92645a84c84bd061'
+#
+#   config.enabled = false if Rails.env.test?
+#
+#   config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
+# end
